72e2560a292ac80bcc9cbaa4f1b1daf62438d048
Tim <ethosscream@yahoo.com>
2017-04-17 02:57:47 -0700

    One round of Bellman-Ford

--- rip_lite_server.py
+++ rip_lite_server.py
@@ -40,11 +40,17 @@ def compute_tables(tables, routes, neighbors):
         # For each table entry
 	for entry in entries:
 	    field = entry.split(',')
+	    cost = costs[neighbor] + int(field[2])
 
             # If the dest was not already in the DV simply add it
             if not field[0] in dests:
-		cost = costs[neighbor] + int(field[2])
                 dests[field[0]] = neighbor + ',' + str(cost)
+                costs[field[0]] = cost
+
+            # Otherwise add if new path is shorter, but don't go negative
+            elif cost < costs[field[0]] and cost > 0:
+                dests[field[0]] = neighbor + ',' + str(cost)
+                costs[field[0]] = cost
 
     # Fill in new DV
     for dest in dests.keys():
